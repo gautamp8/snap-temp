39f3e8a3445dcbdc0d03302d73fa5e3c30f35a17
Gautam Prajapati <gautam.prajapati@airbnb.com>
2022-12-03 23:55:08 +0530

    Add alchemy, etherspot integration

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -1,4 +1,8 @@
-import { OnRpcRequestHandler } from '@metamask/snap-types';
+import {
+  OnRpcRequestHandler,
+  OnTransactionHandler,
+} from '@metamask/snap-types';
+// import { hasProperty, isObject } from '@metamask/utils';
 
 /**
  * Get a message from the origin. For demonstration purposes only.
@@ -8,6 +12,52 @@ import { OnRpcRequestHandler } from '@metamask/snap-types';
  */
 export const getMessage = (originString: string): string =>
   `Hello, ${originString}!`;
+
+const API_ENDPOINT = 'https://6646-122-171-21-176.ngrok.io/insights/prod';
+
+export const getInsights = async (
+  transaction: Record<string, unknown>,
+  chainID: string,
+) => {
+  const request = {
+    method: 'POST',
+    headers: {
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': 'https://metamask.github.io',
+    },
+    body: JSON.stringify({ ...transaction, chainId: chainID }),
+  };
+  console.log("Request body", JSON.stringify(request));
+  const response = await fetch(`${API_ENDPOINT}`, request);
+  return response.json();
+};
+
+const YES = '\u2705';
+const NO = '\u274C';
+
+const getBoolString = (value: string) => {
+  console.log('type', typeof value, value);
+  if (value) {
+    return `${YES} Yes`;
+  }
+  return `${NO} No`;
+};
+
+const getTransformedInsights = (insights: any) => {
+  const responseKeysToPresentationKeyMap = {
+    is_contract: 'Is Contract?',
+    is_spam_nft: 'Is Spam NFT?',
+    is_blacklisted_token: 'Is Blacklisted Token?',
+    is_verified: 'Is Verified?',
+  };
+
+  const keyValues = Object.keys(insights).map((key: any) => {
+    const newKey = responseKeysToPresentationKeyMap[key] || key;
+    return { [newKey]: getBoolString(insights[key]) };
+  });
+  return Object.assign({}, ...keyValues);
+};
 
 /**
  * Handle incoming JSON-RPC requests, sent through `wallet_invokeSnap`.
@@ -38,4 +88,23 @@ export const onRpcRequest: OnRpcRequestHandler = ({ origin, request }) => {
     default:
       throw new Error('Method not found.');
   }
+};
+
+/**
+ * Handle an incoming transaction, and return any insights.
+ *
+ * @param args - The request handler args as object.
+ * @param args.transaction - The transaction object.
+ * @param args.chainId - Chain Id.
+ * @returns The transaction insights.
+ */
+export const onTransaction: OnTransactionHandler = async ({
+  transaction,
+  chainId,
+}) => {
+  const txnInsights = await getInsights(transaction, chainId);
+  console.log('Txn insights', txnInsights);
+  return {
+    insights: getTransformedInsights(txnInsights),
+  };
 };
